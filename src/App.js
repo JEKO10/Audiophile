7e67e22dd3c5db1bd8937eefcd834f7bb92e9a0e
Aleksa Bubanja <aleksa3b@gmail.com>
2021-12-29 22:22:20 +0100

    Collage component setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Recommendation from "./components/Recommendation";
+import Collage from "./components/Collage";
 
 function App() {
   return (
     <main>
       <Header />
       <Recommendation />
+      <Collage />
     </main>
   );
 }
---
+++ src/components/Collage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Speaker from "../images/image-speaker-zx9.png";
+import Earphone from "../images/image-earphones-yx1.jpg";
+
+function Collage() {
+  return (
+    <section className="collage">
+      <div>
+        <img src={Speaker} alt="IMG" />
+        <div>
+          <h1>ZX9 Speaker</h1>
+          <p>
+            Upgrade to premium speakers that are phenomenally built to deliver
+            truly remarkable sound.
+          </p>
+          <button>See Product</button>
+        </div>
+      </div>
+      <div>
+        <h1>ZX7 SPEAKER</h1>
+        <button>See Product</button>
+      </div>
+      <div>
+        <img src={Earphone} alt="" />
+        <h1>YX1 EARPHONES</h1>
+        <button>See Product</button>
+      </div>
+    </section>
+  );
+}
+
+export default Collage;
