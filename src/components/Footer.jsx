796fa1272c27342d65563620b89797843f635e7f
Aleksa Bubanja <aleksa3b@gmail.com>
2021-12-31 19:47:03 +0100

    Footer component finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,34 +5,36 @@ import { FaFacebookSquare, FaTwitter, FaInstagram } from "react-icons/fa";
 function Footer() {
   return (
     <footer>
-      <div>
-        <img src={Logo} alt="Logo" />
-        <p>
-          Audiophile is an all in one stop to fulfill your audio needs. We're a
-          small team of music lovers and sound specialists who are devoted to
-          helping you get the most out of personal audio. Come and visit our
-          demo facility - we’re open 7 days a week.
-        </p>
-        <p>Copyright 2021. All Rights Reserved</p>
-      </div>
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>Headphones</li>
-          <li>Speakers</li>
-          <li>Earphones</li>
-        </ul>
-        <ul>
-          <li>
-            <FaFacebookSquare />
-          </li>
-          <li>
-            <FaTwitter />
-          </li>
-          <li>
-            <FaInstagram />
-          </li>
-        </ul>
+      <div className="container">
+        <div>
+          <img src={Logo} alt="Logo" />
+          <p>
+            Audiophile is an all in one stop to fulfill your audio needs. We're
+            a small team of music lovers and sound specialists who are devoted
+            to helping you get the most out of personal audio. Come and visit
+            our demo facility - we’re open 7 days a week.
+          </p>
+          <p>Copyright 2021. All Rights Reserved</p>
+        </div>
+        <div className="links">
+          <ul className="pages">
+            <li>Home</li>
+            <li>Headphones</li>
+            <li>Speakers</li>
+            <li>Earphones</li>
+          </ul>
+          <ul className="socials">
+            <li>
+              <FaFacebookSquare />
+            </li>
+            <li>
+              <FaTwitter />
+            </li>
+            <li>
+              <FaInstagram />
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
