074f0824f96bcae8ad705b5ab91d82bc95497797
Aleksa Bubanja <aleksa3b@gmail.com>
2022-01-05 22:21:30 +0100

    Headphones page setup

---
+++ src/components/ProductsH.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductsH() {
+  return <div></div>;
+}
+
+export default ProductsH;
---
+++ src/pages/Headphones.js
@@ -0,0 +1,14 @@
+import Recommendation from "../components/Recommendation";
+import ProductsH from "../components/ProductsH";
+
+function Headphones() {
+  return (
+    <div>
+      <h1>Headphones</h1>
+      <ProductsH />
+      <Recommendation />
+    </div>
+  );
+}
+
+export default Headphones;
