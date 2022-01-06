838a0477fffcd41241957d0e88c54626a63fec0a
Aleksa Bubanja <aleksa3b@gmail.com>
2022-01-06 18:52:31 +0100

    products for Headphones setup

--- src/components/ProductsH.jsx
+++ src/components/ProductsH.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import First from "../images/image-xx99-mark-two.jpg";
+import Second from "../images/image-xx99-mark-one.jpg";
+import Third from "../images/image-xx59.jpg";
 
 function ProductsH() {
-  return <div></div>;
+  return (
+    <section className="productsH">
+      <div>
+        <img src={First} alt="IMG" />
+        <div>
+          <h3>New Product</h3>
+          <h1>XX99 MARK II Headphones</h1>
+          <p>
+            The new XX99 Mark II headphones is the pinnacle of pristine audio.
+            It redefines your premium headphone experience by reproducing the
+            balanced depth and precision of studio-quality sound.
+          </p>
+          <button>See Product</button>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h1>XX99 MARK I Headphones</h1>
+          <p>
+            As the gold standard for headphones, the classic XX99 Mark I offers
+            detailed and accurate audio reproduction for audiophiles, mixing
+            engineers, and music aficionados alike in studios and on the go.
+          </p>
+          <button>See Product</button>
+        </div>
+        <img src={Second} alt="IMG" />
+      </div>
+      <div>
+        <img src={Third} alt="IMG" />
+        <div>
+          <h1>XX59 Headphones</h1>
+          <p>
+            Enjoy your audio almost anywhere and customize it to your specific
+            tastes with the XX59 headphones. The stylish yet durable versatile
+            wireless headset is a brilliant companion at home or on the move.
+          </p>
+          <button>See Product</button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ProductsH;
--- src/pages/Headphones.js
+++ src/pages/Headphones.js
@@ -1,12 +1,14 @@
 import Recommendation from "../components/Recommendation";
 import ProductsH from "../components/ProductsH";
+import About from "../components/About";
 
 function Headphones() {
   return (
     <div>
-      <h1>Headphones</h1>
+      <h1 id="titleH">Headphones</h1>
       <ProductsH />
       <Recommendation />
+      <About />
     </div>
   );
 }
