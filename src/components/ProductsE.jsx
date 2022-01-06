fe531af638a55546ec62dc88431ce7b1f5b28b30
Aleksa Bubanja <aleksa3b@gmail.com>
2022-01-06 19:57:33 +0100

    Earphones page setup

---
+++ src/components/ProductsE.jsx
@@ -0,0 +1,26 @@
+import Image from "../images/image-yx1-earphones.jpg";
+
+function ProductsE() {
+  return (
+    <section className="products">
+      <div>
+        <img src={Image} alt="IMG" />
+        <div>
+          <h3>New Product</h3>
+          <h1>
+            YX1 Wireless <br /> Earphones
+          </h1>
+          <p>
+            Tailor your listening experience with bespoke dynamic drivers from
+            the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound
+            even in noisy environments with its active noise cancellation
+            feature.
+          </p>
+          <button>See Product</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ProductsE;
--- src/pages/Earphones.js
+++ src/pages/Earphones.js
@@ -1,7 +1,18 @@
-import React from "react";
+import Recommendation from "../components/Recommendation";
+import ProductsE from "../components/ProductsE";
+import About from "../components/About";
+import Footer from "../components/Footer";
 
 function Earphones() {
-  return <div>Ear</div>;
+  return (
+    <div>
+      <h1 id="title">Earphones</h1>
+      <ProductsE />
+      <Recommendation />
+      <About />
+      <Footer />
+    </div>
+  );
 }
 
 export default Earphones;
