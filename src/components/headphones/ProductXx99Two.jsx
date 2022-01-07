6ce0b8ed55f49437476026bf5f27b2aac61714d2
Aleksa Bubanja <aleksa3b@gmail.com>
2022-01-07 19:32:06 +0100

    features section finished

--- src/components/headphones/ProductXx99Two.jsx
+++ src/components/headphones/ProductXx99Two.jsx
@@ -26,6 +26,48 @@ function ProductXx99Two() {
           </div>
         </div>
       </section>
+      <section className="features">
+        <div>
+          <h1>Features</h1>
+          <p>
+            Featuring a genuine leather head strap and premium earcups, these
+            headphones deliver superior comfort for those who like to enjoy
+            endless listening. It includes intuitive controls designed for any
+            situation. Whether you’re taking a business call or just in your own
+            personal space, the auto on/off and pause features ensure that
+            you’ll never miss a beat.
+          </p>
+          <p>
+            The advanced Active Noise Cancellation with built-in equalizer allow
+            you to experience your audio world on your terms. It lets you enjoy
+            your audio in peace, but quickly interact with your surroundings
+            when you need to. Combined with Bluetooth 5. 0 compliant
+            connectivity and 17 hour battery life, the XX99 Mark II headphones
+            gives you superior sound, cutting-edge technology, and a modern
+            design aesthetic.
+          </p>
+        </div>
+        <div className="box">
+          <h1>In The Box</h1>
+          <ul>
+            <li>
+              <span>1x</span> Headphone Unit
+            </li>
+            <li>
+              <span>2x</span> Replacement Earcups
+            </li>
+            <li>
+              <span>1x</span> User Manual
+            </li>
+            <li>
+              <span>1x</span> 3.5mm 5m Audio Cable
+            </li>
+            <li>
+              <span>1x</span> Travel Bag
+            </li>
+          </ul>
+        </div>
+      </section>
     </>
   );
 }
