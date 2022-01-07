fe9d67d90ece0aea1960a6531e177e204fa1b07e
Aleksa Bubanja <aleksa3b@gmail.com>
2022-01-07 20:13:17 +0100

    Xx99 mark II page finished

--- src/pages/headphones/Xx99.js
+++ src/pages/headphones/Xx99.js
@@ -1,9 +1,15 @@
 import Product from "../../components/headphones/ProductXx99Two";
+import Recommendation from "../../components/Recommendation";
+import About from "../../components/About";
+import Footer from "../../components/Footer";
 
 function xx99() {
   return (
     <div>
       <Product />
+      <Recommendation />
+      <About />
+      <Footer />
     </div>
   );
 }
